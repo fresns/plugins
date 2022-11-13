981b1432ce681947f4c05490db3957aeb87a48e5
Jarvis <jarvis.okay@gmail.com>
2022-11-13 15:48:50 +0800

    chore: removal of old codes

--- old/QiNiu/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('QiNiu::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from plugin: {!! config('qiniu.name') !!}
-    </p>
-@endsection
