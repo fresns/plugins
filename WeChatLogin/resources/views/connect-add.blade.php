38955906e413829423c91619332ee1930a914ceb
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-08-13 20:53:07 +0800

    chore(wechat-login): fresns callback message

--- WeChatLogin/resources/views/connect-add.blade.php
+++ WeChatLogin/resources/views/connect-add.blade.php
@@ -36,15 +36,61 @@
 @endsection
 
 @push('script')
+    <script src="//res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
     <script>
         const isWeChat = {{ $isWeChat ? '1' : '0' }};
         const postMessageKey = '{{ $postMessageKey }}';
         const oauthUrl = '{!! $oauthUrl !!}';
 
         if (isWeChat) {
-            console.log('oauthUrl', oauthUrl);
+            setTimeout(function () {
+                const fresnsCallbackMessage = {
+                    code: 0,
+                    message: 'ok',
+                    action: {
+                        postMessageKey: 'WeChatLogin',
+                        windowClose: true,
+                        redirectUrl: oauthUrl,
+                        dataHandler: ''
+                    },
+                    data: null,
+                }
 
-            window.top.location.href = oauthUrl;
+                const messageString = JSON.stringify(fresnsCallbackMessage);
+                const userAgent = navigator.userAgent.toLowerCase();
+
+                switch (true) {
+                    case (window.Android !== undefined):
+                        // Android (addJavascriptInterface)
+                        window.Android.receiveMessage(messageString);
+                        break;
+
+                    case (window.webkit && window.webkit.messageHandlers.iOSHandler !== undefined):
+                        // iOS (WKScriptMessageHandler)
+                        window.webkit.messageHandlers.iOSHandler.postMessage(messageString);
+                        break;
+
+                    case (window.FresnsJavascriptChannel !== undefined):
+                        // Flutter
+                        window.FresnsJavascriptChannel.postMessage(messageString);
+                        break;
+
+                    case (window.ReactNativeWebView !== undefined):
+                        // React Native WebView
+                        window.ReactNativeWebView.postMessage(messageString);
+                        break;
+
+                    case (userAgent.indexOf('miniprogram') > -1):
+                        // WeChat Mini Program
+                        wx.miniProgram.postMessage({ data: messageString });
+                        wx.miniProgram.navigateBack();
+                        break;
+
+                    // Web
+                    default:
+                        parent.postMessage(messageString, '*');
+                }
+            }, 2000);
         } else {
             $(document).ready(function () {
                 let counter = 0;
